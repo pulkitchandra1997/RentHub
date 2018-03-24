d049853aa10cdf59b86cc07a5f2d5104ca66e5bb
PULKITCHANDRA <chandra.pulkit1997@gmail.com>
2018-03-25 00:39:20 +0530

    -new2

--- app/src/main/java/appp/renthub/Welcome.java
+++ app/src/main/java/appp/renthub/Welcome.java
@@ -10,5 +10,7 @@ public class Welcome extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         //dasdasdasd
+        //sbdkhbaskbdksdbkf
+        //dfksahkf
     }
 }
