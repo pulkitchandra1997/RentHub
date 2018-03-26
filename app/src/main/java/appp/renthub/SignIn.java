298cec5f560360db408321f8faf5abef77dcc67e
pragati2097 <pragatichaturvedi94@gmail.com>
2018-03-27 00:28:31 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/appp/renthub/Welcome.java
    #	app/src/main/res/layout/activity_welcome.xml

---
+++ app/src/main/java/appp/renthub/SignIn.java
@@ -0,0 +1,21 @@
+package appp.renthub;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SignIn extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+
+    public void tenant(View view) {
+    }
+
+    public void owner(View view) {
+    }
+}
