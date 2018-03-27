3240bee0650b4d267adb927b88e5a18ed9d54d69
pragati2097 <pragatichaturvedi94@gmail.com>
2018-03-27 19:02:49 +0530

    Merge branch 'master' of E:\RentHub with conflicts.

--- app/src/main/java/appp/renthub/SignIn.java
+++ app/src/main/java/appp/renthub/SignIn.java
@@ -7,18 +7,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class SignIn extends Activity {
+public class SignIn extends Activity{
+   // Button tenant;
 
-    Button tenant,owner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
-        tenant=findViewById(R.id.tenant);
-        owner=findViewById(R.id.owner);
-        Typeface font = Typeface.createFromAsset(getAssets(), "Font Awesome 5 Free-Solid-900.otf" );
-        owner.setTypeface(font);
-        tenant.setTypeface(font);
+       // tenant=findViewById(R.id.login);
+      //  tenant.setOnClickListener(this);
+       // Typeface font = Typeface.createFromAsset(getAssets(), "Font Awesome 5 Free-Solid-900.otf" );
+       // tenant.setTypeface(font);
+    }
+
+    public void tenant(View view) {
+    }
+
+    public void owner(View view) {
     }
 
 
