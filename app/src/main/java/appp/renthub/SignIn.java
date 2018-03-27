84bd8531b68cee23e3f7a3dbc8c3ab2af4872ab6
pranjal012 <pranjalv206@gmail.com>
2018-03-27 14:30:07 +0530

    signin-pragati

--- app/src/main/java/appp/renthub/SignIn.java
+++ app/src/main/java/appp/renthub/SignIn.java
@@ -1,21 +1,25 @@
 package appp.renthub;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class SignIn extends Activity {
 
+    Button tenant,owner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        tenant=findViewById(R.id.tenant);
+        owner=findViewById(R.id.owner);
+        Typeface font = Typeface.createFromAsset(getAssets(), "Font Awesome 5 Free-Solid-900.otf" );
+        owner.setTypeface(font);
+        tenant.setTypeface(font);
     }
 
-    public void tenant(View view) {
-    }
 
-    public void owner(View view) {
-    }
 }
