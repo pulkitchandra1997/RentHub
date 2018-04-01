d905ca8cbd4f51c89c49ec0622f1917877b6dad9
pranjal012 <pranjalv206@gmail.com>
2018-04-01 12:31:11 +0530

    userprofile-pranjal

--- app/src/main/java/appp/renthub/UserProfile.java
+++ app/src/main/java/appp/renthub/UserProfile.java
@@ -4,12 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
+
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class UserProfile extends Activity {
 
@@ -21,6 +17,8 @@ public class UserProfile extends Activity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+
             switch (item.getItemId()) {
                 case R.id.navigation_search:
                     fragment=new SearchFrag();
@@ -37,10 +35,10 @@ public class UserProfile extends Activity {
                 case R.id.navigation_messages:
                     fragment=new MessageFrag();
                     switchFragment();
-
                     return true;
             }
             return false;
+
         }
     };
 
@@ -54,11 +52,8 @@ public class UserProfile extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
-        ft = getFragmentManager().beginTransaction();
         fragment = new SearchFrag();
-        ft.replace(R.id.content, fragment);
-        ft.commit();
-
+        switchFragment();
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
