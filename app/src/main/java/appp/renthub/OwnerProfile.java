825fe7321f3a7c6ed38c92af91c7a9a2ba56519b
pranjal012 <pranjalv206@gmail.com>
2018-04-05 18:57:51 +0530

    owner-pragati

---
+++ app/src/main/java/appp/renthub/OwnerProfile.java
@@ -0,0 +1,60 @@
+package appp.renthub;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class OwnerProfile extends AppCompatActivity {
+
+    android.app.Fragment fragment = null;
+    android.app.FragmentTransaction ft;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+
+            switch (item.getItemId()) {
+                case R.id.navigation_post:
+                    fragment=new SearchFrag();
+                    switchFragment();
+                    return true;
+                case R.id.navigation_account:
+                    fragment=new AccountFrag();
+                    switchFragment();
+                    return true;
+                case R.id.navigation_manage:
+                    fragment=new ManageFrag();
+                    switchFragment();
+                    return true;
+                case R.id.navigation_messages:
+                    fragment=new MessageFrag();
+                    switchFragment();
+                    return true;
+            }
+            return false;
+
+        }
+    };
+
+    public void switchFragment()
+    {
+        ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.content, fragment);
+        ft.commit();
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owner_profile);
+        fragment = new PostFrag();
+        switchFragment();
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+}
