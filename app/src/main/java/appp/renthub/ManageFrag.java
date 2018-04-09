055e40da7f423a91fc5dc321281d16c8adc7b3da
AyushiDaksh <aayusheedaksh@gmail.com>
2018-04-10 03:17:08 +0530

    placeDetailsFrag edited- ayushi

--- app/src/main/java/appp/renthub/ManageFrag.java
+++ app/src/main/java/appp/renthub/ManageFrag.java
@@ -1,5 +1,6 @@
 package appp.renthub;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,6 +8,10 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * Created by pranj on 01-04-2018.
@@ -16,7 +21,8 @@ public class ManageFrag extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view= inflater.inflate(R.layout.managefrag,container,false);
+        View view= inflater.inflate(R.layout.placedetailsfrag,container,false);
+
 
         return view;
     }
